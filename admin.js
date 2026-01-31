a56655efa9ef80406dc8caa43682c61f64e2b70a
tranvanthanhblog <tranvanthanhblog@gmail.com>
2026-01-31 20:33:15 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,19 +1,17 @@
 DB.auth.onAuthStateChanged((user) => {
-  // CHƯA ĐĂNG NHẬP
+
   if (!user) {
     alert("Vui lòng đăng nhập trước");
     location.href = "index.html";
     return;
   }
 
-  // KHÔNG PHẢI ADMIN
   if (!DB.isAdminEmail(user.email)) {
     alert("Không có quyền admin");
     location.href = "index.html";
     return;
   }
 
-  // NẾU ĐÚNG ADMIN → CHO Ở LẠI TRANG
   console.log("Đã đăng nhập admin:", user.email);
 });
 
@@ -28,4 +26,5 @@ btnPublish.onclick = () => {
     location.href = "index.html";
   });
 };
+
 
