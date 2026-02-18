d671c8f50750fbc9729cf16ee0b3b78452bcfb82
tranvanthanhblog <tranvanthanhblog@gmail.com>
2026-02-18 14:36:05 +0700

    Update db.js

--- db.js
+++ db.js
@@ -63,7 +63,7 @@ function addComment(postId, user, text) {
   return db.ref(`posts/${postId}/comments/${id}`).set({
     uid: user.uid,
     name: user.displayName,
-    photo: <img src="avatar.jpg">,
+    photo: <img src="avatar.jpg" />,
     text,
     createdAt: Date.now(),
   });
@@ -90,6 +90,7 @@ window.DB = {
   addComment,
   onComments,
 };
+
 
 
 
